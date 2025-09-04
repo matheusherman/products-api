565b6521312957db849e7bb4daf3fdbdb6c17e48
Matheus Herman <matheushbandrade@hotmail.com>
2025-09-04 11:07:11 -0300

    endpoints creation

--- src/routes/product_router.rs
+++ src/routes/product_router.rs
@@ -1,2 +1,13 @@
-use crate::dto::product_dto::{CreateProduct, UpdateProduct};
-use crate::models::product::Product;
+use actix_web::web;
+
+use crate::handlers::product_handler;
+
+pub fn config_product_routes(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("/products")
+            .route("", web::post().to(product_handler::create_product))
+            .route("/{id}", web::get().to(product_handler::get_product))
+            .route("/{id}", web::patch().to(product_handler::patch_product))
+            .route("/{id}", web::delete().to(product_handler::delete_product)),
+    );
+}
